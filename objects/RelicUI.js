25d251a7e5a6714746e4d0ad841dab8991a3419c
Nicalai Guy <nicalaitguy@gmail.com>
2025-10-09 17:56:59 -0500

    Refine relic tray presentation

--- objects/RelicUI.js
+++ objects/RelicUI.js
@@ -7,31 +7,53 @@ export class RelicUIManager {
         this.relicBackgrounds = new Map();
         this.relicInfoTitleText = null;
         this.relicInfoDescriptionText = null;
+        this.relicInfoBackground = null;
+        this.relicTrayBackground = null;
         this.selectedRelicId = null;
     }
 
     reset() {
         this.clearVisuals();
         this.destroyInfoTexts();
+        this.destroyInfoBackground();
+        this.destroyTrayBackground();
         this.selectedRelicId = null;
     }
 
     createShelf() {
         this.clearVisuals();
         this.destroyInfoTexts();
+        this.destroyInfoBackground();
+        this.destroyTrayBackground();
 
-        this.relicInfoTitleText = this.scene.add.text(CONSTANTS.RIGHT_COLUMN_X, CONSTANTS.RELIC_INFO_TITLE_Y, '', {
+        const infoCenterX = CONSTANTS.RIGHT_COLUMN_X - CONSTANTS.RELIC_INFO_WRAP_WIDTH / 2;
+        const infoWidth = CONSTANTS.RELIC_INFO_WRAP_WIDTH + CONSTANTS.RELIC_INFO_BACKGROUND_PADDING;
+
+        this.relicInfoBackground = this.scene.add.rectangle(
+            infoCenterX,
+            CONSTANTS.RELIC_INFO_TITLE_Y,
+            infoWidth,
+            CONSTANTS.RELIC_INFO_BACKGROUND_HEIGHT,
+            0x0b0b0b,
+            0.55
+        )
+            .setOrigin(0.5, 0)
+            .setStrokeStyle(2, 0xf1c40f, 0.35);
+
+        this.relicInfoTitleText = this.scene.add.text(infoCenterX, CONSTANTS.RELIC_INFO_TITLE_Y, '', {
             fontSize: '24px',
             color: '#f1c40f',
-            fontStyle: 'bold'
-        }).setOrigin(1, 0);
+            fontStyle: 'bold',
+            align: 'center'
+        }).setOrigin(0.5, 0);
 
-        this.relicInfoDescriptionText = this.scene.add.text(CONSTANTS.RIGHT_COLUMN_X, CONSTANTS.RELIC_INFO_TITLE_Y + 32, '', {
+        this.relicInfoDescriptionText = this.scene.add.text(infoCenterX, CONSTANTS.RELIC_INFO_TITLE_Y + 36, '', {
             fontSize: '18px',
             color: '#f9e79f',
             wordWrap: { width: CONSTANTS.RELIC_INFO_WRAP_WIDTH },
-            lineSpacing: 6
-        }).setOrigin(1, 0);
+            lineSpacing: 6,
+            align: 'center'
+        }).setOrigin(0.5, 0);
 
         this.setInfoText('', '');
         this.updateDisplay();
@@ -50,22 +72,52 @@ export class RelicUIManager {
         const baseY = CONSTANTS.RELIC_TRAY_Y;
         const spacing = CONSTANTS.RELIC_ICON_SPACING;
         const iconSize = CONSTANTS.RELIC_ICON_SIZE;
+        const maxSlots = CONSTANTS.RELIC_MAX_SLOTS;
+        const trayWidth = (maxSlots - 1) * spacing + iconSize;
+        const trayCenterX = startX - ((maxSlots - 1) * spacing) / 2;
+        const trayWidthWithPadding = trayWidth + CONSTANTS.RELIC_INFO_BACKGROUND_PADDING;
+        const trayHeightWithPadding = iconSize + CONSTANTS.RELIC_INFO_BACKGROUND_PADDING;
 
-        ownedRelics.forEach((relic, index) => {
+        this.destroyTrayBackground();
+        this.relicTrayBackground = this.scene.add.rectangle(
+            trayCenterX,
+            baseY,
+            trayWidthWithPadding,
+            trayHeightWithPadding,
+            0x0b0b0b,
+            0.55
+        )
+            .setStrokeStyle(2, 0xf1c40f, 0.4);
+        this.relicVisuals.push(this.relicTrayBackground);
+
+        for (let index = 0; index < maxSlots; index += 1) {
+            const relic = ownedRelics[index] || null;
             const x = startX - index * spacing;
-            const iconBg = this.scene.add.rectangle(x, baseY, iconSize, iconSize, 0x1c1c1c, 0.85)
-                .setStrokeStyle(2, 0xf1c40f, 0.9)
-                .setInteractive({ useHandCursor: true });
-            const iconText = this.scene.add.text(x, baseY, relic.icon || '♦', {
-                fontSize: CONSTANTS.RELIC_ICON_FONT_SIZE
-            }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+            const iconBg = this.scene.add.rectangle(
+                x,
+                baseY,
+                iconSize,
+                iconSize,
+                0x1c1c1c,
+                relic ? 0.85 : 0.25
+            ).setStrokeStyle(2, 0xf1c40f, relic ? 0.9 : 0.35);
 
-            iconBg.on('pointerdown', () => this.showRelicDetails(relic));
-            iconText.on('pointerdown', () => this.showRelicDetails(relic));
+            this.relicVisuals.push(iconBg);
 
-            this.relicVisuals.push(iconBg, iconText);
-            this.relicBackgrounds.set(relic.id, iconBg);
-        });
+            if (relic) {
+                iconBg.setInteractive({ useHandCursor: true });
+
+                const iconText = this.scene.add.text(x, baseY, relic.icon || '♦', {
+                    fontSize: CONSTANTS.RELIC_ICON_FONT_SIZE
+                }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+
+                iconBg.on('pointerdown', () => this.showRelicDetails(relic));
+                iconText.on('pointerdown', () => this.showRelicDetails(relic));
+
+                this.relicVisuals.push(iconText);
+                this.relicBackgrounds.set(relic.id, iconBg);
+            }
+        }
 
         const selectedRelic = ownedRelics.find(relic => relic.id === this.selectedRelicId);
         if (selectedRelic) {
@@ -107,6 +159,7 @@ export class RelicUIManager {
         this.relicVisuals.forEach(setVisibility);
         setVisibility(this.relicInfoTitleText);
         setVisibility(this.relicInfoDescriptionText);
+        setVisibility(this.relicInfoBackground);
     }
 
     clearVisuals() {
@@ -123,6 +176,7 @@ export class RelicUIManager {
         } else {
             this.relicBackgrounds = new Map();
         }
+        this.relicTrayBackground = null;
     }
 
     destroyInfoTexts() {
@@ -133,6 +187,20 @@ export class RelicUIManager {
         if (this.relicInfoDescriptionText) {
             this.relicInfoDescriptionText.destroy();
             this.relicInfoDescriptionText = null;
+        }
+    }
+
+    destroyInfoBackground() {
+        if (this.relicInfoBackground) {
+            this.relicInfoBackground.destroy();
+            this.relicInfoBackground = null;
+        }
+    }
+
+    destroyTrayBackground() {
+        if (this.relicTrayBackground) {
+            this.relicTrayBackground.destroy();
+            this.relicTrayBackground = null;
         }
     }
 
